07b6042b8e8c59808f1725737395c99e67de6e91
Josue Hernandez <josuehdz0@gmail.com>
2023-03-22 15:21:07 -0600

    first

---
+++ checkpoint7allspice.client/src/env.js
@@ -0,0 +1,10 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:7045' : ''
+export const useSockets = false
+export const domain = 'josuehdz0.us.auth0.com'
+export const clientId = '5FoHweUt7qPYObh7UIG9KlCiw38fTTAZ'
+export const audience = 'https://nube.com'
+
+
+
+
---
+++ checkpoint7allspice/Models/Recipe.cs
@@ -0,0 +1,12 @@
+namespace checkpoint7allspice.Models
+{
+  public class Recipe
+  {
+    public int Id { get; set; }
+    public string CreatorId { get; set; }
+    public string Title { get; set; }
+    public string Instructions { get; set; }
+    public string Category { get; set; }
+    public Profile Creator { get; set; }
+  }
+}
